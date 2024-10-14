ddd368708b86f34f5ec3d52dc511def8dcdeede1
nileshparwan <nileshparwan.utm.bse16b@gmail.com>
2024-10-14 13:24:46 +0400

    implement desktop sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import Sidebar from '@/components/Sidebar';
+
 export default function RootLayout({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {firstName: 'Koshal', lastName: 'Parwan'};
   return (
-    <main>
-      sidebar
+    <main className='flex h-screen w-full font-inter'>
+      <Sidebar user={loggedIn}/>
       {children}
     </main>
   );
